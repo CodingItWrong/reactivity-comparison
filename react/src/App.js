c3f3d218321c794844f5f53bcc98e7bb00a9345c
Josh Justice <josh.justice@accesscfa.com>
2021-01-08 07:46:01 -0500

    Add todo toggling to React

--- react/src/App.js
+++ react/src/App.js
@@ -2,26 +2,48 @@ import {useState} from 'react';
 import './App.css';
 
 function App() {
-  const [newWidgetName, setNewWidgetName] = useState('');
-  const [widgets, setWidgets] = useState([]);
+  const [newTodoName, setNewTodoName] = useState('');
+  const [todos, setTodos] = useState([]);
 
-  const handleAddWidget = e => {
+  const handleAddTodo = e => {
     e.preventDefault();
 
+    const todo = { name: newTodoName, complete: false };
+
     // React docs don't actually even give an example of an array `useState` value???
-    setWidgets([...widgets, newWidgetName]);
-    setNewWidgetName('');
+    setTodos([...todos, todo]);
+    setNewTodoName('');
+  }
+
+  const toggleComplete = index => {
+    const todo = todos[index];
+    const updatedTodo = { ...todo, complete: !todo.complete };
+    setTodos([
+      ...todos.slice(0, index),
+      updatedTodo,
+      ...todos.slice(index + 1),
+    ]);
   }
 
   return (
     <div>
-      <h1>Widgets</h1>
-      <form onSubmit={handleAddWidget}>
-        <input type="text" placeholder="Widget" value={newWidgetName} onChange={e => setNewWidgetName(e.target.value)} />
+      <h1>Todos</h1>
+      <form onSubmit={handleAddTodo}>
+        <input type="text" placeholder="Todo" value={newTodoName} onChange={e => setNewTodoName(e.target.value)} />
         <button type="submit">Add</button>
       </form>
       <ul>
-        {widgets.map(widget => (<li>{widget}</li>))}
+        {todos.map((todo, index) => (
+          <li>
+            {todo.name}
+            -
+            {todo.complete ? 'Complete' : 'Incomplete'}
+            -
+            <button onClick={() => toggleComplete(index)}>
+              Toggle
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
