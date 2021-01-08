3a882ff13691c0656ea7f689db1c1be2d96ee12a
Josh Justice <josh.justice@accesscfa.com>
2021-01-08 07:37:50 -0500

    Convert ember to todos

--- ember/app/components/todo-list.js
+++ ember/app/components/todo-list.js
@@ -2,22 +2,22 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
-export default class WidgetListComponent extends Component {
-  @tracked newWidgetName = '';
-  @tracked widgets = [];
+export default class TodoListComponent extends Component {
+  @tracked newTodoName = '';
+  @tracked todos = [];
 
   @action
-  handleAddWidget(e) {
+  handleAddTodo(e) {
     e.preventDefault();
 
     // https://guides.emberjs.com/release/upgrading/current-edition/tracked-properties/#toc_arrays
     // Arrays are another example of a type of object where you can't enumerate every possible value - after all, there are an infinite number of integers (though you may run out of bits in your computer at some point!). Instead, you can continue to use EmberArray, which will continue to work with tracking and will cause any dependencies that use it to invalidate correctly.
     // doesn't work:
-    // this.widgets.push(this.newWidgetName);
+    // this.todos.push(this.newTodoName);
 
     // works:
-    this.widgets = [...this.widgets, this.newWidgetName];
+    this.todos = [...this.todos, this.newTodoName];
 
-    this.newWidgetName = '';
+    this.newTodoName = '';
   }
 }
