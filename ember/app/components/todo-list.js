8725c961eb6abfdcd786130e167802a6c734c463
Josh Justice <josh.justice@accesscfa.com>
2021-01-08 07:38:56 -0500

    Add complete flag

--- ember/app/components/todo-list.js
+++ ember/app/components/todo-list.js
@@ -10,14 +10,34 @@ export default class TodoListComponent extends Component {
   handleAddTodo(e) {
     e.preventDefault();
 
+    const todo = { name: this.newTodoName, complete: false };
+
     // https://guides.emberjs.com/release/upgrading/current-edition/tracked-properties/#toc_arrays
     // Arrays are another example of a type of object where you can't enumerate every possible value - after all, there are an infinite number of integers (though you may run out of bits in your computer at some point!). Instead, you can continue to use EmberArray, which will continue to work with tracking and will cause any dependencies that use it to invalidate correctly.
     // doesn't work:
     // this.todos.push(this.newTodoName);
 
     // works:
-    this.todos = [...this.todos, this.newTodoName];
+    this.todos = [...this.todos, todo];
 
     this.newTodoName = '';
+  }
+
+  @action
+  toggleComplete(index) {
+    const todo = this.todos[index];
+
+    // doesn't work
+    // todo.complete = !todo.complete;
+
+    // necessary
+    const updatedTodo = { ...todo, complete: !todo.complete };
+    this.todos = [
+      ...this.todos.slice(0, index),
+      updatedTodo,
+      ...this.todos.slice(index + 1),
+    ];
+
+    console.log(this.todos);
   }
 }
